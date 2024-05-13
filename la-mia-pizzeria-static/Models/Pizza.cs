ff1360549918a4dae5046516ae52c748de31ada8
Fonta97 <fontana.alessandro97@gmail.com>
2024-05-13 13:58:19 +0200

    scusa mi son dimenticato il push Andre :(

---
+++ la-mia-pizzeria-static/Models/Pizza.cs
@@ -0,0 +1,20 @@
+﻿﻿namespace la_mia_pizzeria_static.Models
+{
+    public class Pizza
+    {
+        public string _name { get; set; }
+        public string _description { get; set; }
+        public string _img { get; set; }
+        public decimal _price { get; set; }
+
+        public Pizza() { }
+
+        public Pizza(string name, string description, string img, decimal price)
+        {
+            _name = name;
+            _description = description;
+            _img = img;
+            _price = price;
+        }
+    }
+}
